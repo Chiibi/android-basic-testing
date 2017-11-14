887963268ae5aaf0b4948a67914bc90c73087f71
Chiibi <Chiibi_jame@hotmail.com>
2017-11-15 01:27:07 +0700

    preparing and eating a cookie :cookie:
    - add RuleException class
    - add Validator class

---
+++ app/src/main/java/cc/somkiat/basicunittesting/CustomException/RuleException.java
@@ -0,0 +1,11 @@
+package cc.somkiat.basicunittesting.CustomException;
+
+/**
+ * Created by Chiib_000 on 15/11/2560.
+ */
+
+public class RuleException extends Exception {
+    public RuleException(String message) {
+        super(message);
+    }
+}
---
+++ app/src/main/java/cc/somkiat/basicunittesting/Validator/NameValidator.java
@@ -0,0 +1,34 @@
+package cc.somkiat.basicunittesting.Validator;
+
+import java.util.regex.Pattern;
+
+import cc.somkiat.basicunittesting.CustomException.RuleException;
+
+/**
+ * Created by Chiib_000 on 15/11/2560.
+ */
+
+public class NameValidator {
+
+    public NameValidator(){
+
+    }
+
+    public boolean validate(String name) throws RuleException{
+        isNotNullOrEmpty(name);
+        isEngLanguage(name);
+        return true;
+    }
+
+    public void isEngLanguage(String name) throws RuleException {
+        if (!Pattern.matches("^[a-zA-Z]+$", name)){
+            throw new RuleException("Name pattern miss match");
+        }
+    }
+
+    public void isNotNullOrEmpty(String name) throws RuleException {
+        if (name.isEmpty()){
+            throw new RuleException("Name is empty");
+        }
+    }
+}
