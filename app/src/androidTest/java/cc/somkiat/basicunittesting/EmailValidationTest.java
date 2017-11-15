2b62b152390044874a15e13792dfcbcc62370e3d
Chiibi <Chiibi_jame@hotmail.com>
2017-11-15 16:07:05 +0700

    [Add Test] Email empty

---
+++ app/src/androidTest/java/cc/somkiat/basicunittesting/EmailValidationTest.java
@@ -0,0 +1,23 @@
+package cc.somkiat.basicunittesting;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import cc.somkiat.basicunittesting.CustomException.RuleException;
+import cc.somkiat.basicunittesting.Validator.EmailValidator;
+
+/**
+ * Created by Chiib_000 on 15/11/2560.
+ */
+@RunWith(AndroidJUnit4.class)
+public class EmailValidationTest {
+
+    @Test(expected = RuleException.class)
+    public void emailIsEmpty() throws RuleException {
+        String email = "";
+        EmailValidator emailValidator = new EmailValidator();
+        emailValidator.isNotNullOrEmpty(email);
+    }
+}
